9fbdb00a41d9ecfe30cf4211183b2de61b8988fd
rajeev <rajeevchoudhary2425@gmail.com>
2024-12-31 18:10:32 +0530

    updated Form.jsx

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
 import {useNavigate,useLocation} from 'react-router-dom'
 import axios from 'axios'
 import './Form.css';
@@ -9,24 +9,32 @@ function Form(){
     const[startDate,setStartDate]=useState('');
     const[EndDate,setEndDate]=useState('');
     const[people,setPeople]=useState('');
+    const[tripType,setTripType]=useState('');
+    const[amount,setAmount]=useState(0);
+    const[packageInformation,setPackageInformation]=useState({});
     const navigate=useNavigate();
+    useEffect(()=>{
+        async function getPackageInformation(){
+            const information=await axios.get(`${backend}/api/users/packagedetail/${location.state.id}`);
+            setPackageInformation(information.data[0]);
+        }
+        getPackageInformation();
+    },[]);
     async function generateInvoice(e){
         e.preventDefault();
         try{
             const user_id=localStorage.getItem('id');
-            const information=await axios.get(`${backend}/api/users/packagedetail/${location.state.id}`);
-            console.log(information.data);
-            const result=await axios.post(`${backend}/api/users/booking`,[user_id,location.state.id,email,startDate,EndDate,people,information.data[0].place,information.data[0].name,information.data[0].image_url,information.data[0].amount]);
+            const result=await axios.post(`${backend}/api/users/booking`,[user_id,location.state.id,email,startDate,EndDate,people,packageInformation.place,packageInformation.name,packageInformation.image_url,amount]);
             console.log(result.data);
             navigate('/invoice',{
                 state:{
                     booking_id:result.data[0].id,
                     user_id:user_id,
                     package_id:location.state.id,
-                    place:information.data[0].place,
-                    name:information.data[0].name,
-                    image_url:information.data[0].image_url,
-                    amount:information.data[0].amount,
+                    place:packageInformation.place,
+                    name:packageInformation.name,
+                    image_url:packageInformation.image_url,
+                    amount:amount,
                     email,
                     startDate,
                     EndDate,
@@ -37,6 +45,22 @@ function Form(){
         }catch(err){
             console.error(err.message);
         }
+    }
+    async function handleTripType(e){
+        setTripType(e.target.value);
+        if(tripType==='honeymoon'){
+            setPeople(2);
+            setAmount(packageInformation.amount*2);
+        }else if(tripType==='solo Trip'){
+            setPeople(1);
+            setAmount(packageInformation.amount);
+        }else{
+            setPeople('');
+        }
+    }
+    async function setFamilyTripPeople(e){
+        setPeople(e.target.value);
+        setAmount(packageInformation.amount*e.target.value);
     }
     return (
         <div className='container'>
@@ -71,13 +95,43 @@ function Form(){
                         required/>
                     </div>
                     <div className='labelAndDate'>
-                        <label htmlFor="NO. of People">No. of People:</label>
+                        <label htmlFor="Email">Trip Type:</label>
+                        <select className='select-trip-type'
+                            value={tripType}
+                            onChange={handleTripType}
+                            required >
+                            <option value='' disabled>Select Trip Type And No. of People</option>
+                            <option value='family Trip'>Family Trip</option>
+                            <option value='solo Trip'>Solo Trip</option>
+                            <option value='honeymoon'>Honeymoon</option>
+                        </select>
+                    </div>
+                    {tripType==='family Trip' && (
+                        <div className='labelAndDate'>
+                            <label htmlFor="NO. of People">No. of People:</label>
+                            <input type='number'
+                            className='input-people'
+                            placeholder='No. of People'
+                            value={people}
+                            onChange={setFamilyTripPeople}
+                            required/>
+                        </div>
+                    )}
+                    {(tripType==='solo Trip' || tripType==='honeymoon') && (
+                        <div className='labelAndDate'>
+                            <label htmlFor="NO. of People">No. of People:</label>
+                            <input type='number'
+                            className='input-people'
+                            value={people}
+                            readOnly/>
+                        </div>
+                    )}
+                    <div className='labelAndDate'>
+                        <label htmlFor="Total Amount">Total Amount:</label>
                         <input type='number'
-                        className='input-people'
-                        placeholder='No. of People'
-                        value={people}
-                        onChange={(e)=>setPeople(e.target.value)}
-                        required/>
+                        className='total-amount'
+                        value={amount}
+                        readOnly/>
                     </div>
                     <button className='submit-btn' type='submit'>Confirm Booking</button>
                     <button className='cancel-btn' onClick={()=>navigate('/dashboard')}>Cancel</button>
